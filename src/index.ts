b38682ac91127d4644b4bebfd628996ba02114c5
juniklee <dlrl0326@naver.com>
2023-01-18 15:41:41 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+const showcase = document.getElementById('showcase') as HTMLIFrameElement;
+const key = 'a627793955d0ec6d';
+console.log(showcase);
+// declare this file is a module
+export {};
+
+// augment window with the MP_SDK property
+declare global {
+  interface Window {
+    MP_SDK: any;
+  }
+}
+
+showcase.addEventListener('load', async function() {
+  let sdk;
+  try {
+    sdk = await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6');
+  }
+  catch(e) {
+    console.error(e);
+    return;
+  }
+
+  console.log('%c  Hello Bundle SDK! ', 'background: #333333; color: #00dd00');
+  console.log(sdk);
+});
